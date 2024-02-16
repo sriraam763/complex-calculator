68ed8005bdc74cc5d60a4bf8b39a23090e6125c9
Junaid Chaudhry <bricksigma176@gmail.com>
2024-02-16 08:14:44 +0300

    File containing button text for scientific calculator

---
+++ src/ui/ButtonTexts.java
@@ -0,0 +1,10 @@
+package ui;
+
+public class ButtonTexts {
+    public static String x_sqr = "<html>x<sup style='font-size:20pt'>2<sup></html>";
+    public static String x_cube = "<html>x<sup style='font-size:20pt'>3<sup></html>";
+    public static String pow = "<html>x<sup style='font-size:20pt'>y<sup></html>";
+    public static String e_pow = "<html>e<sup style='font-size:20pt'>x<sup></html>";
+    public static String ten_sqr = "<html>10<sup style='font-size:20pt'>x<sup></html>";
+    public static String inverse = "<html>x<sup>-1</sup></html>";
+}
