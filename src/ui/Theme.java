fa5b908a26abb9c547b9f2bd9f6ff6ce6fddab83
Junaid Chaudhry <bricksigma176@gmail.com>
2024-02-23 09:41:24 +0300

    Updated background color

--- src/ui/Theme.java
+++ src/ui/Theme.java
@@ -3,7 +3,7 @@ package ui;
 import java.awt.*;
 
 public class Theme {
-    public static final Color BG_COLOR = new Color(50, 50, 47);
+    public static final Color BG_COLOR = new Color(36, 36, 36);
     public static final Color FG_COLOR = Color.WHITE;
     public static final Color PRIMARY_COLOR = new Color(255, 160, 11);
     public static final Color SECONDARY_COLOR = new Color(100, 100, 98);
